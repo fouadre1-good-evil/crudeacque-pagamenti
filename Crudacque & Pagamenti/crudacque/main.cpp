0556161656910c0e4429be034fed9258be584597
wMickyy <micky09yt@gmail.com>
2026-05-12 19:08:37 +0200

    Varie sistemazioni

---
+++ Crudacque & Pagamenti/crudacque/main.cpp
@@ -0,0 +1,546 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <conio.h>
+#include <windows.h>
+#define MAX 500
+#define MAX_NOME 120
+#define ESC 27
+#define ENTER 13
+#define SU  72
+#define DX  77
+#define GIU  80
+#define SX 75
+#define R 114
+
+using namespace std;
+
+struct Archivio {
+    char utente [MAX_NOME];
+    char codiceFiscale [16];
+    char indirizzo [MAX_NOME];
+    double trimestreAttuale;
+    double trimestrePrecedente;
+    double bolletteNonPagate;
+    bool attivo;
+};
+
+Archivio tab [MAX];
+Archivio inputArchivio ();
+
+void inputStringa (int maxval, char mex[], char s[]);
+void inputTabella (Archivio tab [], int &posAttuale);
+void creaFile ();
+void modificaMenu(Archivio &t);
+void gotoXY(int x, int y);
+void graficaMenu (Archivio t, int b, int bp, bool inserimento);
+void hideCursor();
+void showCursor();
+void menu(int &n);
+
+int lunghezzaNumero(double n);
+int inputInteroSenzaMax (int min, char mex[]);
+int inputIntero (int min, int max, char mex[]);
+
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    system("color B0");
+
+    ifstream fLettura("TRIMESTRE.bin", ios::binary | ios::ate);
+    int posAttuale = 0;
+
+    if (!fLettura.is_open()) {
+        creaFile();
+    } else {
+        long dimensioneFile = fLettura.tellg();
+        posAttuale = dimensioneFile / sizeof(Archivio);
+        fLettura.seekg(0, ios::beg);
+        for (int i = 0; i < posAttuale; i++) {
+            fLettura.read((char *)&tab[i], sizeof(Archivio));
+        }
+        fLettura.close();
+    }
+
+    system("cls");
+    menu(posAttuale);
+}
+
+void inputStringa (int maxval, char mex[], char s[]) {
+    do {
+        cout << mex;
+        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        cin.getline (s, maxval);
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+    } while (strlen(s) == 0);
+}
+
+int inputInteroSenzaMax (int min, char mex[]) {
+    int n;
+    do {
+        cout << mex;
+        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        cin >> n;
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+    } while (n < min);
+    return n;
+}
+
+int inputIntero (int min, int max, char mex[]) {
+    int n;
+    do {
+        cout << mex << endl;
+        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        cin >> n;
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+    } while (n < min || n >= max);
+    return n;
+}
+
+Archivio inputArchivio () {
+    Archivio t;
+    inputStringa (MAX_NOME, " Digitare il nome e il cognome: ", t.utente);
+    inputStringa (16, " Digitare il codice fiscale: ", t.codiceFiscale);
+    inputStringa (MAX_NOME, " Digitare l'indirizzo di casa: ", t.indirizzo);
+    t.trimestreAttuale = inputInteroSenzaMax (0, " Digitare la lettura del contatore alla fine del trimestre attuale (in metri cubi): ");
+    t.trimestrePrecedente = inputInteroSenzaMax(0, " Digitare la lettura del contatore alla fine del trimestre precedente (in metri cubi): ");
+    t.bolletteNonPagate = inputInteroSenzaMax(0, " Digitare l'importo di eventuali bollette precedenti non pagate: ");
+    t.attivo = true;
+    return t;
+}
+
+void inputTabella (Archivio tab [], int &posAttuale) {
+    for (int i = posAttuale; i < posAttuale+1; i++) {
+        cout << " Inserisci i dati per l'utente numero " << i+1 << ":" << endl;
+        cout << endl;
+        tab[i] = inputArchivio ();
+    }
+}
+
+void creaFile () {
+    ofstream fTrimestre;
+    fTrimestre.open("TRIMESTRE.bin", ios::binary);
+    if (!fTrimestre.is_open()) {
+        perror ("Errore nell'apertura del file: ");
+    }
+    fTrimestre.close();
+}
+
+void menu(int &posAttuale) {
+    int i=0;
+    fstream fTrimestre;
+    int ip;
+    bool inMenu = true;
+    while (inMenu) {
+        hideCursor();
+        gotoXY(1,1);
+        cout << "Selezione come vuoi procedere. Usa le frecce SU/GIU e premi INVIO.\n";
+        gotoXY(1,10);
+        cout << "Premi ESC per uscire.\n\n";
+        gotoXY(2,3);
+        cout << "┌───────────────────────────┐";
+        for (int i=0; i<4; i++) {
+            gotoXY(2,4+i);
+            cout << "│";
+            gotoXY(30, 4+i);
+            cout << "│";
+        }
+        gotoXY(2,8);
+        cout << "└───────────────────────────┘";
+        gotoXY(3,4);
+        cout << "- ";
+        if (i == 0) {
+            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        }
+        gotoXY(5,4);
+        cout << "Inserire un nuovo utente";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        gotoXY(3,5);
+        cout << "- ";
+        if (i == 1) {
+            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        }
+        gotoXY(5,5);
+        cout << "Modificare un utente";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        gotoXY(3,6);
+        cout << "- ";
+        if (i == 2) {
+            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        }
+        gotoXY(5,6);
+        cout << "Cancellare un utente";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        gotoXY(3,7);
+        cout << "- ";
+        if (i == 3) {
+            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        }
+        gotoXY(5,7);
+        cout << "Visualizzare gli utenti";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+
+        gotoXY(0,ip+4);
+        cout << "  ";
+        gotoXY(0,i+4);
+        cout <<"->";
+
+        ip = i;
+        char tasto = _getch();
+        if (tasto == ESC) {
+            inMenu = false;
+        }
+        if (tasto == SU) {
+            if(i == 0) {
+                i = 3;
+            } else {
+                i-=1;
+            }
+        }
+        if (tasto == GIU) {
+            if(i == 3) {
+                i = 0;
+            } else {
+                i+=1;
+            }
+        }
+        if (tasto == ENTER) {
+            cin.sync();
+            showCursor();
+            system("cls");
+            switch (i) {
+            case 0: {
+                bool reinserimento = true;
+                char risposta;
+                fTrimestre.open("TRIMESTRE.bin", ios::binary | ios::out | ios::app);
+                if(!fTrimestre.is_open()) {
+                    perror("Errore:");
+                } else {
+                    while (reinserimento) {
+                        system("cls");
+                        inputTabella(tab,posAttuale);
+                        fTrimestre.write((char *)&tab[posAttuale], sizeof(Archivio));
+                        posAttuale++;
+                        cout << endl;
+                        do {
+                            cout << " Vuoi inserire un altro utente? (y/n)" << endl;
+                            cout << " ";
+                            cin >> risposta;
+                            cin.ignore(100, '\n');
+                        } while (risposta != 'y' && risposta != 'Y' && risposta != 'n' && risposta != 'N');
+                        if (risposta == 'n' || risposta == 'N') {
+                            reinserimento = false;
+                        }
+                        system("cls");
+                    }
+                }
+                fTrimestre.close();
+            }
+            break;
+            case 1: {
+                fTrimestre.open("TRIMESTRE.bin",ios::binary|ios::in|ios::out);
+                if(!fTrimestre.is_open()) {
+                    perror("Errore:");
+                } else {
+                    int posScelta;
+                    system("cls");
+                    gotoXY(1,1);
+                    cout << "Quale utente si vuole modificare? (da 1 a " << posAttuale << ")" << endl;
+                    gotoXY(1,2);
+                    cin >>posScelta;
+                    cin.ignore(100, '\n');
+                    Archivio t;
+                    fTrimestre.seekg((posScelta-1)*sizeof(Archivio), ios::beg);
+                    if (fTrimestre.read((char *)&t, sizeof(Archivio))) {
+                        system("cls");
+                        modificaMenu(t);
+                        system("cls");
+                        fTrimestre.seekp((-1) * sizeof(Archivio), ios::cur);
+                        fTrimestre.write((char *)&t, sizeof(Archivio));
+                        tab[posScelta - 1] = t;
+                        hideCursor();
+                        gotoXY(1,1);
+                        cout << "Modifica salvata sul file!";
+                        Sleep(1500);
+                    } else {
+                        hideCursor();
+                        gotoXY(1,4);
+                        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+                        cout << "Utente non trovato!";
+                        Sleep(2000);
+                        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+                        system("cls");
+                    }
+                }
+                fTrimestre.close();
+                break;
+            }
+            case 2:
+                fTrimestre.open("TRIMESTRE.bin",ios::binary|ios::in|ios::out);
+                if(!fTrimestre.is_open()) {
+                    perror("Errore:");
+                } else {
+
+                }
+                break;
+            case 3:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+int lunghezzaNumero(double n) {
+    int len = 0;
+    int intero = (int)n;
+
+    if (intero == 0) {
+        len = 1;
+    } else {
+        int temp = intero;
+        while (temp > 0) {
+            len++;
+            temp /= 10;
+        }
+    }
+
+    if (n - intero > 0) {
+        len += 2;
+    }
+    return len;
+}
+
+void modificaMenu(Archivio &t) {
+    int b = 4;
+    int bp = b;
+    bool inMenu = true;
+    while (inMenu) {
+        hideCursor();
+        graficaMenu(t, b, bp, false);
+        bp = b;
+        char tasto = _getch();
+        if (tasto == ESC) {
+            inMenu = false;
+        }
+        if (tasto == SU) {
+            if(b == 4) {
+                b = 9;
+            } else {
+                b-=1;
+            }
+        }
+        if (tasto == GIU) {
+            if(b == 9) {
+                b = 4;
+            } else {
+                b+=1;
+            }
+        }
+        if (tasto == ENTER) {
+            cin.sync();
+            showCursor();
+            system("cls");
+            switch (b) {
+            case 4:
+                strcpy (t.utente, "");
+                graficaMenu(t, b, bp, true);
+                gotoXY(2, 10);
+                cout << endl;
+                cout << "Inserisci il nuovo utente";
+                gotoXY(13, 4);
+                inputStringa(MAX_NOME, "", t.utente);
+                break;
+            case 5:
+                strcpy (t.codiceFiscale, "");
+                graficaMenu(t, b, bp, true);
+                gotoXY(2, 10);
+                cout << endl;
+                cout << "Inserisci il nuovo Codice Fiscale";
+                gotoXY(21, 5);
+                inputStringa(16, "", t.codiceFiscale);
+                break;
+            case 6:
+                strcpy (t.indirizzo, "");
+                graficaMenu(t, b, bp, true);
+                gotoXY(2, 10);
+                cout << endl;
+                cout << "Inserisci il nuovo indirizzo";
+                gotoXY(16, 6);
+                inputStringa(MAX_NOME, "", t.indirizzo);
+                break;
+            case 7:
+                graficaMenu(t, b, bp, true);
+                for (int i=0; i<lunghezzaNumero(t.trimestreAttuale)+3; i++) { // Quel +3 è per far cancellare anche il " mq"
+                    gotoXY(36+i, 7);
+                    cout << " ";
+                }
+                gotoXY(2, 10);
+                cout << endl;
+                cout << "Nuova lettura attuale (mq)";
+                gotoXY(36, 7);
+                t.trimestreAttuale = inputInteroSenzaMax(0, "");
+                break;
+            case 8:
+                graficaMenu(t, b, bp, true);
+                for (int i=0; i<lunghezzaNumero(t.trimestrePrecedente)+3; i++) { // Quel +3 è per far cancellare anche il " mq"
+                    gotoXY(39+i, 8);
+                    cout << " ";
+                }
+                gotoXY(2, 10);
+                cout << endl;
+                cout << "Nuova lettura precedente (mq)";
+                gotoXY(39, 8);
+                t.trimestrePrecedente = inputInteroSenzaMax(0, "");
+                break;
+            case 9:
+                graficaMenu(t, b, bp, true);
+                for (int i=0; i<lunghezzaNumero(t.bolletteNonPagate)+5; i++) { // Quel +5 è per far cancellare anche il " euro"
+                    gotoXY(40+i, 9);
+                    cout << " ";
+                }
+                gotoXY(2, 10);
+                cout << endl;
+                cout << "Nuovo importo bollette (euro)";
+                gotoXY(40, 9);
+                t.bolletteNonPagate = inputInteroSenzaMax(0, "");
+                break;
+            }
+        }
+    }
+}
+
+void graficaMenu (Archivio t, int b, int bp, bool inserimento) {
+    gotoXY(1,1);
+    cout << "Cosa vuoi modificare? Usa le frecce SU/GIU e premi INVIO.\n";
+    gotoXY(1,2);
+    cout << "Premi ESC per uscire.\n\n";
+    //Calcolo della lunghezza di ogni singola riga
+    int lunghezze[6];
+    lunghezze[0] = 5  + strlen(t.utente);
+    lunghezze[1] = 13 + strlen(t.codiceFiscale);
+    lunghezze[2] = 8  + strlen(t.indirizzo);
+    lunghezze[3] = 31 + lunghezzaNumero(t.trimestreAttuale);
+    lunghezze[4] = 34 + lunghezzaNumero(t.trimestrePrecedente);
+    lunghezze[5] = 37 + lunghezzaNumero(t.bolletteNonPagate);
+
+    //Trovo qualle piu grande
+    int maxLen = lunghezze[0];
+    for (int i = 1; i < 6; i++) {
+        if (lunghezze[i] > maxLen) {
+            maxLen = lunghezze[i];
+        }
+    }
+    gotoXY(2,3);
+    cout << "┌";
+    for(int j = 0; j < maxLen + 6; j++) {
+        cout << "─";
+    }
+    cout << "┐\n";
+
+    for (int i = 0; i < 6; i++) {
+        gotoXY(2,4+i);
+        cout << "│";
+
+        if (i == 0) {
+            gotoXY(3,4);
+            cout << "- ";
+            if (b == 4) {
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+            }
+            gotoXY(5,4);
+            cout << "Utente: " << t.utente << "";
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        } else if (i == 1) {
+            gotoXY(3,5);
+            cout << "- ";
+            if (b == 5) {
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+            }
+            gotoXY(5,5);
+            cout << "Codice Fiscale: " << t.codiceFiscale << "";
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        } else if (i == 2) {
+            gotoXY(3,6);
+            cout << "- ";
+            if (b == 6) {
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+            }
+            gotoXY(5,6);
+            cout << "Indirizzo: " << t.indirizzo << "";
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        } else if (i == 3) {
+            gotoXY(3,7);
+            cout << "- ";
+            if (b == 7) {
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+            }
+            gotoXY(5,7);
+            cout << "Lettura del trimestre attuale: " << t.trimestreAttuale << " mq";
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        } else if (i == 4) {
+            gotoXY(3,8);
+            cout << "- ";
+            if (b == 8) {
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+            }
+            gotoXY(5,8);
+            cout << "Lettura del trimestre precedente: " << t.trimestrePrecedente << " mq";
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        } else if (i == 5) {
+            gotoXY(3,9);
+            cout << "- ";
+            if (b == 9) {
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+            }
+            gotoXY(5,9);
+            cout << "Importo delle bollette non pagate: " << t.bolletteNonPagate << " euro";
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        }
+
+        int spaziMancanti = maxLen - lunghezze[i];
+        for(int j = 0; j < spaziMancanti; j++) {
+            cout << " ";
+        }
+        if (!inserimento) {
+            cout << " │\n";
+        }
+
+    }
+    gotoXY(0,bp);
+    cout << "  ";
+    gotoXY(0,b);
+    cout <<"->";
+    gotoXY(2,10);
+    cout << "└";
+    for(int j = 0; j < maxLen + 6; j++) {
+        cout << "─";
+    }
+    cout << "┘\n";
+}
+
+void gotoXY(int x, int y) {
+    COORD coordinate;
+    coordinate.X = x;
+    coordinate.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);
+}
+
+void hideCursor () {
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(console, &cursorInfo);
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(console, &cursorInfo);
+}
+
+void showCursor() {
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(console, &cursorInfo);
+    cursorInfo.bVisible = TRUE;
+    SetConsoleCursorInfo(console, &cursorInfo);
+}
