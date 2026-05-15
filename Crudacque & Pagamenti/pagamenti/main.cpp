7729b317cd5cc21cd4daba297ac4f148a0a24f95
Fouad <fouadre2317@gmail.com>
2026-05-15 20:57:23 +0200

    sucate

--- Crudacque & Pagamenti/pagamenti/main.cpp
+++ Crudacque & Pagamenti/pagamenti/main.cpp
@@ -30,27 +30,165 @@ struct Archivio {
 };
 
 void creazioneBolletta (ifstream &fLettura);
+void caricamento();
+void gotoXY(int x, int y);
+void hideCursor ();
+
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main() {
+    keybd_event(VK_MENU, 0x36,0,0);                  // ---------|
+    keybd_event(VK_RETURN, 0x1c,0,0);                //          | ------> Per mettere lo
+    keybd_event(VK_RETURN, 0x1c,KEYEVENTF_KEYUP,0);  //          | ------> schermo intero all'avvio
+    keybd_event(VK_MENU, 0x38,KEYEVENTF_KEYUP,0);    // ---------|
+    SetConsoleOutputCP(CP_UTF8); // ------> Per i simboli
+    SetConsoleCP(CP_UTF8);       // ------> delle carte
+    system("color B0");
+    hideCursor();
     ifstream fLettura("../TRIMESTRE.bin", ios::binary);
-
     if (!fLettura.is_open()) {
         cout << "Errore: file binario non trovato." << endl;
     } else {
         creazioneBolletta(fLettura);
         fLettura.close();
-        cout << "Elaborazione terminata." << endl;
+        caricamento();
     }
     return 0;
+}
+
+void caricamento() {
+    gotoXY(0,10);
+    cout << R"(
+                                                                 ___   ___   _____ _____ _____ ___  _  _ ___       ___       ___       _
+                                                                / __| /_\ \ / / __|_   _|_   _/ _ \| \| |_ _|     / __|     | _ \     | |
+                                                               | (_ |/ _ \ V /| _|  | |   | || (_) | .` || |      \__ \  _  |   /  _  | |__
+                                                                \___/_/ \_\_/ |___| |_|   |_| \___/|_|\_|___|     |___/ (_) |_|_\ (_) |____|
+
+)";
+gotoXY(0,27);
+cout << R"(
+                                                                                                  ___
+                                                                                              ,--[___]--,
+                                                                                             /           \
+                                                                                            |,.--'```'--.,|
+                                                                                            |'-.,_____,.-'|
+                                                                                            |'-.,_____,.-'|
+                                                                                            |             |
+                                                                                            |  W A T E R  |
+                                                                                            |             |
+                                                                                            |'-.,_____,.-'|
+                                                                                            `'-.,_____,.-''
+)";
+    gotoXY(70,20);
+    cout << "┌────────────────────────────────────────────────────────────┐";
+    gotoXY(70,21);
+    cout << "│                                                            │";
+    gotoXY(70,22);
+    cout << "└────────────────────────────────────────────────────────────┘";
+    for (int i = 0; i < 60 ; i++) {
+        if(i <= 20) {
+            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_INTENSITY);
+        } else if(i <= 40) {
+            for (int j = 0 ;j<=i;j++){
+                SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY);
+                gotoXY(71+j,21);
+                cout<<" ";
+            }
+            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY);
+        } else {
+            for (int j = 0 ;j<=i;j++){
+                SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_INTENSITY);
+                gotoXY(71+j,21);
+                cout<<" ";
+            }
+            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_INTENSITY);
+        }
+        gotoXY(71+i,21);
+        cout << " ";
+        Sleep(1);
+    }
+    int x;
+    system("color B0");
+    gotoXY(0,54);
+    cin >>x;
+    gotoXY(0,54);
+    cout << "                                               ";
+    if(x == 67){
+        cout << R"(
+
+)";
+    gotoXY(188,54);
+    cout << "┌───────────────────┐";
+    Sleep(500);
+    gotoXY(188,54);
+    cout << "                          ";
+    gotoXY(188,54);
+    cout << "│Achievement unlocked│";
+    gotoXY(188,53);
+    cout << "┌────────────────────┐";
+    Sleep(500);
+    gotoXY(188,54);
+    cout << "                         ";
+    gotoXY(188,52);
+    cout << "                          ";
+    gotoXY(188,54);
+    cout << "│Name:???            │";
+    gotoXY(188,52);
+    cout << "┌────────────────────┐";
+    gotoXY(188,53);
+    cout << "│Achievement unlocked│";
+    Sleep(500);
+    gotoXY(187,51);
+    cout << "                          ";
+    gotoXY(187,52);
+    cout << "                          ";
+    gotoXY(187,54);
+    cout << "                          ";
+    gotoXY(187,53);
+    cout << "                          ";
+    gotoXY(187,52);
+    cout << "                          ";
+    gotoXY(187,53);
+    cout << "│Name:???            │";
+    gotoXY(187,51);
+    cout << "┌────────────────────┐";
+    gotoXY(187,52);
+    cout << "│Achievement unlocked│";
+    gotoXY(187,54);
+    cout << "│Description:67      │";
+    Sleep(500);
+    gotoXY(187,54);
+    cout << "                         ";
+    gotoXY(187,53);
+    cout << "                          ";
+    gotoXY(187,52);
+    cout << "                         ";
+    gotoXY(187,51);
+    cout << "                          ";
+    gotoXY(187,50);
+    cout << "                          ";
+    gotoXY(187,49);
+    cout << "                          ";
+    gotoXY(187,52);
+    cout << "│Name:???            │";
+    gotoXY(187,50);
+    cout << "┌────────────────────┐";
+    gotoXY(187,51);
+    cout << "│Achievement unlocked│";
+    gotoXY(187,53);
+    cout << "│Description:67      │";
+    gotoXY(187,54);
+    cout << "└────────────────────┘";
+    }
+    char c;
+    c = _getch();
 }
 
 void creazioneBolletta (ifstream &fLettura) {
     Archivio t;
     char nomeFile[25];
-
     while (fLettura.read((char *)&t, sizeof(Archivio))) {
-
-        if (t.attivo) {
+        if (!t.attivo) {
             double consumiPeriodo = t.trimestreAttuale - t.trimestrePrecedente;
             double quotaFissa = 25.00;
             double quotaVariabile = consumiPeriodo * 2.80;
@@ -66,11 +204,9 @@ void creazioneBolletta (ifstream &fLettura) {
             strcpy(nomeFile, "../");
             strcat(nomeFile, t.codiceFiscale);
             strcat(nomeFile, ".TXT");
-
             ofstream fBolletta(nomeFile);
             if (fBolletta.is_open()) {
                 fBolletta << fixed << setprecision(2);
-
                 fBolletta << "Nome:                         " << t.utente << endl;
                 fBolletta << "Codice Fiscale:               " << t.codiceFiscale << endl;
                 fBolletta << "Indirizzo:                    " << t.indirizzo << endl;
@@ -80,10 +216,23 @@ void creazioneBolletta (ifstream &fLettura) {
                 fBolletta << "Quota fissa:                  " << quotaFissa << endl;
                 fBolletta << "Quota variabile:              " << quotaVariabile << endl;
                 fBolletta << "Totale da pagare:             " << totaleDaPagare << endl;
-
                 fBolletta.close();
-                cout << "File creato: " << nomeFile << endl;
             }
         }
     }
+}
+
+void gotoXY(int x, int y) {
+    COORD coordinate;
+    coordinate.X = x;
+    coordinate.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);
+}
+
+void hideCursor () {
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(console, &cursorInfo);
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(console, &cursorInfo);
 }
